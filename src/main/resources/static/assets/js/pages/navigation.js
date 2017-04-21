a2548e3fd955d11a285cf43cef45715742fc90e7
Oleksii Palagin <apalagin@outlook.com>
2017-04-21 07:59:27 -0700

    Prototyping navigation
    * added metrics for tag table (min/max/avg);
    * start prototyping navigable caption
    * introduced TODO list to not forget ideas :)

--- src/main/resources/static/assets/js/pages/navigation.js
+++ src/main/resources/static/assets/js/pages/navigation.js
@@ -1,11 +1,24 @@
 $("#nav-content").load("navigation/tags.html");
 
+var caption = "TAGS";
+
 function loadTagValues(tagId) {
-$("#nav-content").load("navigation/values.html?tagId=" + tagId);
-$("#nav-caption").text(tagId);
+    $("#nav-content").load("navigation/values.html?tagId=" + tagId);
+    caption += "&nbsp;&gt;&nbsp;" + tagId;
+    updateCaption(caption);
 }
 
 function loadTags(query) {
-$("#nav-content").load("navigation/tags.html?q=query");
-$("#nav-caption").text(query);
+    $("#nav-content").load("navigation/tags.html?q=query");
+    caption += "&nbsp;&gt;&nbsp;" + query;
+    updateCaption(caption);
+}
+
+function updateCaption(caption) {
+    caption = addRefs(caption);
+    $("#nav-caption").html(caption);
+}
+
+function addRefs(caption) {
+    return caption;
 }
